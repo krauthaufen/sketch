9370af160da83b0529a2f5aa0db82bec0fa4d8f0
aszabo <aszabo@vrvis.at>
2019-01-22 17:07:59 +0100

    [Gasac] sync

--- src/Gasac.fsx
+++ src/Gasac.fsx
@@ -18,72 +18,6 @@ type Specimen<'a> =
         override x.GetHashCode() =
             x.genome.GetHashCode()
 
-//module Normal =
-//    let private table =
-//        [|
-//            0.50000; 0.50399; 0.50798; 0.51197; 0.51595; 0.51994; 0.52392; 0.52790; 0.53188; 0.53586
-//            0.53983; 0.54380; 0.54776; 0.55172; 0.55567; 0.55966; 0.56360; 0.56749; 0.57142; 0.57535
-//            0.57926; 0.58317; 0.58706; 0.59095; 0.59483; 0.59871; 0.60257; 0.60642; 0.61026; 0.61409
-//            0.61791; 0.62172; 0.62552; 0.62930; 0.63307; 0.63683; 0.64058; 0.64431; 0.64803; 0.65173
-//            0.65542; 0.65910; 0.66276; 0.66640; 0.67003; 0.67364; 0.67724; 0.68082; 0.68439; 0.68793
-//            0.69146; 0.69497; 0.69847; 0.70194; 0.70540; 0.70884; 0.71226; 0.71566; 0.71904; 0.72240
-//            0.72575; 0.72907; 0.73237; 0.73565; 0.73891; 0.74215; 0.74537; 0.74857; 0.75175; 0.75490
-//            0.75804; 0.76115; 0.76424; 0.76730; 0.77035; 0.77337; 0.77637; 0.77935; 0.78230; 0.78524
-//            0.78814; 0.79103; 0.79389; 0.79673; 0.79955; 0.80234; 0.80511; 0.80785; 0.81057; 0.81327
-//            0.81594; 0.81859; 0.82121; 0.82381; 0.82639; 0.82894; 0.83147; 0.83398; 0.83646; 0.83891
-//            0.84134; 0.84375; 0.84614; 0.84849; 0.85083; 0.85314; 0.85543; 0.85769; 0.85993; 0.86214
-//            0.86433; 0.86650; 0.86864; 0.87076; 0.87286; 0.87493; 0.87698; 0.87900; 0.88100; 0.88298
-//            0.88493; 0.88686; 0.88877; 0.89065; 0.89251; 0.89435; 0.89617; 0.89796; 0.89973; 0.90147
-//            0.90320; 0.90490; 0.90658; 0.90824; 0.90988; 0.91149; 0.91308; 0.91466; 0.91621; 0.91774
-//            0.91924; 0.92073; 0.92220; 0.92364; 0.92507; 0.92647; 0.92785; 0.92922; 0.93056; 0.93189
-//            0.93319; 0.93448; 0.93574; 0.93699; 0.93822; 0.93943; 0.94062; 0.94179; 0.94295; 0.94408
-//            0.94520; 0.94630; 0.94738; 0.94845; 0.94950; 0.95053; 0.95154; 0.95254; 0.95352; 0.95449
-//            0.95543; 0.95637; 0.95728; 0.95818; 0.95907; 0.95994; 0.96080; 0.96164; 0.96246; 0.96327
-//            0.96407; 0.96485; 0.96562; 0.96638; 0.96712; 0.96784; 0.96856; 0.96926; 0.96995; 0.97062
-//            0.97128; 0.97193; 0.97257; 0.97320; 0.97381; 0.97441; 0.97500; 0.97558; 0.97615; 0.97670
-//            0.97725; 0.97778; 0.97831; 0.97882; 0.97932; 0.97982; 0.98030; 0.98077; 0.98124; 0.98169
-//            0.98214; 0.98257; 0.98300; 0.98341; 0.98382; 0.98422; 0.98461; 0.98500; 0.98537; 0.98574
-//            0.98610; 0.98645; 0.98679; 0.98713; 0.98745; 0.98778; 0.98809; 0.98840; 0.98870; 0.98899
-//            0.98928; 0.98956; 0.98983; 0.99010; 0.99036; 0.99061; 0.99086; 0.99111; 0.99134; 0.99158
-//            0.99180; 0.99202; 0.99224; 0.99245; 0.99266; 0.99286; 0.99305; 0.99324; 0.99343; 0.99361
-//            0.99379; 0.99396; 0.99413; 0.99430; 0.99446; 0.99461; 0.99477; 0.99492; 0.99506; 0.99520
-//            0.99534; 0.99547; 0.99560; 0.99573; 0.99585; 0.99598; 0.99609; 0.99621; 0.99632; 0.99643
-//            0.99653; 0.99664; 0.99674; 0.99683; 0.99693; 0.99702; 0.99711; 0.99720; 0.99728; 0.99736
-//            0.99744; 0.99752; 0.99760; 0.99767; 0.99774; 0.99781; 0.99788; 0.99795; 0.99801; 0.99807
-//            0.99813; 0.99819; 0.99825; 0.99831; 0.99836; 0.99841; 0.99846; 0.99851; 0.99856; 0.99861
-//            0.99865; 0.99869; 0.99874; 0.99878; 0.99882; 0.99886; 0.99889; 0.99893; 0.99896; 0.99900
-//            0.99903; 0.99906; 0.99910; 0.99913; 0.99916; 0.99918; 0.99921; 0.99924; 0.99926; 0.99929
-//            0.99931; 0.99934; 0.99936; 0.99938; 0.99940; 0.99942; 0.99944; 0.99946; 0.99948; 0.99950
-//            0.99952; 0.99953; 0.99955; 0.99957; 0.99958; 0.99960; 0.99961; 0.99962; 0.99964; 0.99965
-//            0.99966; 0.99968; 0.99969; 0.99970; 0.99971; 0.99972; 0.99973; 0.99974; 0.99975; 0.99976
-//            0.99977; 0.99978; 0.99978; 0.99979; 0.99980; 0.99981; 0.99981; 0.99982; 0.99983; 0.99983
-//            0.99984; 0.99985; 0.99985; 0.99986; 0.99986; 0.99987; 0.99987; 0.99988; 0.99988; 0.99989
-//            0.99989; 0.99990; 0.99990; 0.99990; 0.99991; 0.99991; 0.99992; 0.99992; 0.99992; 0.99992
-//            0.99993; 0.99993; 0.99993; 0.99994; 0.99994; 0.99994; 0.99994; 0.99995; 0.99995; 0.99995
-//            0.99995; 0.99995; 0.99996; 0.99996; 0.99996; 0.99996; 0.99996; 0.99996; 0.99997; 0.99997
-//            0.99997; 0.99997; 0.99997; 0.99997; 0.99997; 0.99997; 0.99998; 0.99998; 0.99998; 0.99998
-//            1.0
-//        |]
-
-//    let rec cdf (v : float) =
-//        if v = 0.0 then 
-//            0.5
-//        elif v > 0.0 then
-//            if v < 4.1 then
-//                let i = (v / 4.1) * (float table.Length - 1.0)
-//                let i0 = int i
-//                let i1 = i0 + 1
-//                let f = i - float i0
-//                printfn "%.3f" f
-//                let v0 = table.[i0]
-//                let v1 = table.[i1]
-
-//                v0 + (v1 - v0) * f
-//            else 
-//                1.0
-//        else
-//            1.0 - cdf -v
-
 type RansacResult<'d, 't, 's> = 
     { 
         data            : 'd[]
@@ -100,19 +34,30 @@ let inline sq a = a * a
 
 module Utils = 
     let rand = RandomSystem()
-    let getRandomIndex (probs : array<float>) =
-        let pick = rand.UniformDouble() * (probs |> Array.sum)
+    let getRandomIndices (n : int) (weights : array<float>) =
+        
+        let probs = weights.Copy()
+        let res = Array.zeroCreate n
 
-        let mutable cdf = 0.0
-        let mutable found = false
-        let mutable i = 0
-        while not found do
-            cdf <- cdf + probs.[i]
-            if pick <= cdf then
-                found <- true
-            else            
-                i <- i+1        
-        i
+        for j in 0..n-1 do
+            let pick = rand.UniformDouble() * (probs |> Array.sum)
+
+            let mutable cdf = 0.0
+            let mutable found = false
+            let mutable i = 0
+            while not found do
+                cdf <- cdf + probs.[i]
+                if pick <= cdf then
+                    found <- true
+                else            
+                    i <- i+1        
+            
+            res.[j] <- i
+            probs.[i] <- 0.0
+
+        res |> Array.toSeq
+
+    let getRandomIndex = getRandomIndices 1 >> Seq.head
 
 type Gasac =
     
@@ -124,7 +69,7 @@ type Gasac =
             construct :  'a[] -> list<'b>, 
             countInliers : 'b -> 't[] -> int,  
             getInliers : 'b -> 't[] -> int[], 
-            getRandomTrainIndex : unit -> int,
+            getRandomTrainIndices : int -> int[],
             train : 'a[], 
             test : 't[] 
             ) : Option<_> =
@@ -178,7 +123,7 @@ type Gasac =
                     }
             
         let newSpecimen() = 
-            let gen = Array.init needed (fun _ -> getRandomTrainIndex())
+            let gen = getRandomTrainIndices 4
             if hasDuplicates gen then   
                 None
             else
@@ -205,7 +150,7 @@ type Gasac =
             let mutated = s.genome
             for i in 0..needed-1 do
                 if rand.UniformDouble() < mutationProb then
-                    mutated.[i] <- getRandomTrainIndex()
+                    mutated.[i] <- (getRandomTrainIndices 1 |> Seq.head)
 
             birthSpecimen mutated
                  
@@ -274,14 +219,14 @@ module Gasac =
 module Test =
     
     let rnd() =
-        let xs = [|0;1;2;3;4|]
-        let ps = [|100.0;1.0;1.0;1.0;1.0|]
+        let ps = Array.append [|100.0|] (Array.replicate 100 1.0)
 
-        let cts = Array.zeroCreate xs.Length
-        let ct = 100000
+        let cts = Array.zeroCreate ps.Length
+        let ct = 1000000
         for i in 0..ct-1 do
-            let el = Utils.getRandomIndex ps
-            cts.[el] <- cts.[el] + 1
+            let els = Utils.getRandomIndices 5 ps
+            for el in els do
+                cts.[el] <- cts.[el] + 1
 
         let cts = cts |> Array.map (fun e -> float e / float ct)
         printfn "probs: "
@@ -302,15 +247,14 @@ module Test =
             let n = V2d(ray.Direction.Y, -ray.Direction.X)
             Plane2d(n,p)
             
+
+
         let superpts = 
             let t = rand.UniformDouble() + 0.25
             let p1 = ray.GetPointOnRay(t)
             let p2 = ray.GetPointOnRay(-t)
             [|p1; p2|]
             
-        let evil = 
-            Array.init 5000 ( fun _ -> v2r() + V2d(2.0,2.0))
-
         let pts = 
             let r = 
                 Array.init 5000 ( fun _ -> v2r())
@@ -322,12 +266,12 @@ module Test =
                     let norm = V2d(ray.Direction.Y, -ray.Direction.X)
                     p + norm * off
                 )
-            Array.concat [l;r;evil;superpts]
+            Array.concat [superpts;l;r]
 
         let probs =
             Array.init pts.Length (fun i -> 
-                if i >= pts.Length-2 then
-                    100.0
+                if i < superpts.Length then
+                    10.0
                 else
                     1.0                
             )
@@ -360,8 +304,8 @@ module Test =
 
         let getInliers ((p0,p1,ps) : V2d * V2d * Plane2d) pts = inliers ps pts
 
-        let getRandom () =
-            Utils.getRandomIndex probs
+        let getRandom (n : int) =
+            Utils.getRandomIndices n probs |> Seq.toArray
 
         match Gasac.solve 0.999 0.05 2 construct countInliers getInliers getRandom pts pts with
         | None -> ()
